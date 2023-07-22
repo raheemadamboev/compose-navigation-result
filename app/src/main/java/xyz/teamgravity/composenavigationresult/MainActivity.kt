e4e8f8f77946818d5f80c32f710eb7449f43f7c3
raheemadamboev <vividricemaryandmax@gmail.com>
2023-07-22 17:49:21 +0500

    Setup Material3;

--- app/src/main/java/xyz/teamgravity/composenavigationresult/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/composenavigationresult/MainActivity.kt
@@ -6,13 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.composenavigationresult.ui.theme.ComposeNavigationResultTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
